765c5587d2ac0bfbaf74cdd1ba3726213ad8d056
Allison Mackey <a.mackey693@gmail.com>
2020-04-27 14:25:27 -0700

    add functionality to landing page with a button to show/hide divs. add drop down menu with radius selection options. add initial functionality to receive user input info.

--- src/main.js
+++ src/main.js
@@ -2,61 +2,26 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { MapApi } from './maps-service-service';
+import { MapApi } from './maps-service';
 
-const sampleResponse = {
-  "total": 8228,
-  "businesses": [
-    {
-      "rating": 4,
-      "price": "$",
-      "phone": "+14152520800",
-      "id": "E8RJkjfdcwgtyoPMjQ_Olg",
-      "alias": "four-barrel-coffee-san-francisco",
-      "is_closed": false,
-      "categories": [
-        {
-          "alias": "coffee",
-          "title": "Coffee & Tea"
-        }
-      ],
-      "review_count": 1738,
-      "name": "Four Barrel Coffee",
-      "url": "https://www.yelp.com/biz/four-barrel-coffee-san-francisco",
-      "coordinates": {
-        "latitude": 37.7670169511878,
-        "longitude": -122.42184275
-      },
-      "image_url": "http://s3-media2.fl.yelpcdn.com/bphoto/MmgtASP3l_t4tPCL1iAsCg/o.jpg",
-      "location": {
-        "city": "San Francisco",
-        "country": "US",
-        "address2": "",
-        "address3": "",
-        "state": "CA",
-        "address1": "375 Valencia St",
-        "zip_code": "94103"
-      },
-      "distance": 1604.23,
-      "transactions": ["pickup", "delivery"]
-    },
-    // ...
-  ],
-  "region": {
-    "center": {
-      "latitude": 37.767413217936834,
-      "longitude": -122.42820739746094
-    }
-  }
-}
+$(document).ready(() => {
+  // Button for Landing Page
+  $("#enter").click(function() {
+    $("#user-info").show();
+    $("#safety").hide();
+  });
+  // Button for user to "submit" form
+  $("#user-input").submit(function(event) {
+    event.prevntDefault();
+    const zipCode = parseInt($("#zip").val());
+    const miles = parseInt($("#miles").val());
+  
 
-$(document).ready((event) => {
-  event.preventDefault();
-  $("#form").submit(() => {
+    // console.log(zipCode);
     (async () => {
       let mapApi = new MapApi();
-      const response = await mapApi.getBuisnesses()
+      const response = await mapApi.getBuisnesses();
       console.log(response);
-    })()
+    })();
   });
 });
